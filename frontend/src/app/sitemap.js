702cd756f249e2b9236ccd57af7e3d6dbd332d21
Vanshahuja1 <139003501+Vanshahuja1@users.noreply.github.com>
2025-10-03 18:18:48 +0530

    changes

--- frontend/src/app/sitemap.js
+++ frontend/src/app/sitemap.js
@@ -4,6 +4,22 @@ export default function sitemap() {
         url: 'https://qruzine.com',
         lastModified: new Date(),
       },
+      {
+        url: 'https://qruzine.com/login',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://qruzine.com/landing',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://qruzine.com/landing/privacy',
+        lastModified: new Date(),
+      },
+      {
+        url: 'https://qruzine.com/landing/about',
+        lastModified: new Date(),
+      },
     ]
   }
   
